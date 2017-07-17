d782a01b2ea2345ce1a284fd87b1e93c8eafa111
Emanuele Sorce <emanuele.sorce@hotmail.com>
2017-07-17 16:01:19 +0200

    Update package.sh

--- package.sh
+++ package.sh
@@ -9,7 +9,7 @@ cp minetest.apparmor package
 cp -r misc package
 cp -r mods package
 cp -r textures package
-cp -r doc package
+cp -r po package
 cp -r fonts package
 cp minetest-startup.sh package
 click build package
