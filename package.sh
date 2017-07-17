0b0fbedc68ff709293fe43d1a55bb3a6a8a7a3e7
Emanuele Sorce <emanuele.sorce@hotmail.com>
2017-07-17 15:24:51 +0200

    Create package.sh

---
+++ package.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+mkdir package
+cp -r bin package
+cp -r sharedobjects package
+cp -r games package
+cp app.desktop package
+cp manifest.json package
+cp minetest.apparmor package
+cp -r misc package
+cp -r mods package
+cp -r textures package
+cp -r docs package
+cp -r fonts package
+cp minetest-startup.sh package
+click build package
+
