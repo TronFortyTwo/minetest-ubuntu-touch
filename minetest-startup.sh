b62addf9851815bc6fe5f5c468f1123a0e68574b
Emanuele Sorce <emanuele.sorce@hotmail.com>
2017-07-18 04:59:10 -0600

    Saving space on the .cache directory
    Copying there only the things really needed

--- minetest-startup.sh
+++ minetest-startup.sh
@@ -2,8 +2,13 @@
 LD_LIBRARY_PATH="sharedobjects/"
 if [ ! -f /home/phablet/.cache/minetest.emanuelesorce/debug.txt ]; then
     mkdir /home/phablet/.cache/minetest.emanuelesorce
-    echo "First launch! Copying files..."
-    cp -r * /home/phablet/.cache/minetest.emanuelesorce/
+    echo "First launch! Installing files..."
+    cp -r bin /home/phablet/.cache/minetest.emanuelesorce/
+    cp -r games /home/phablet/.cache/minetest.emanuelesorce/
+    cp -r mods /home/phablet/.cache/minetest.emanuelesorce/
+    cp -r textures /home/phablet/.cache/minetest.emanuelesorce/
+    cp -r po /home/phablet/.cache/minetest.emanuelesorce/
+    cp -r fonts /home/phablet/.cache/minetest.emanuelesorce/
 fi
 cd /home/phablet/.cache/minetest.emanuelesorce
 exec bin/minetest
